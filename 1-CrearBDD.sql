559a720d17f5810a914c4c00549597fe1393985a
rorquera <arckanhell1@gmail.com>
2024-10-19 12:24:07 -0500

    creacion de una base de datos en postgres

---
+++ 1-CrearBDD.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE evaluacion2
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Spanish_Colombia.1252'
+    LC_CTYPE = 'Spanish_Colombia.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
